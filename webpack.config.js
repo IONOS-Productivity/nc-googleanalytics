38c8fb1ec79a6ef5a071a9e620211566d6109881
Thomas Lehmann <t.lehmann@strato-rz.de>
2023-12-12 11:57:18 +0100

    Remove unused Webpack build configs and scripts
    Not used in this addon since it does not build Javascript.

--- webpack.config.js
+++
@@ -1,5 +0,0 @@
-// SPDX-FileCopyrightText: Mikhailo Matiyenko-Kupriyanov <kupriyanov+nextcloud@strato.de>
-// SPDX-License-Identifier: AGPL-3.0-or-later
-const webpackConfig = require('@nextcloud/webpack-vue-config')
-
-module.exports = webpackConfig
