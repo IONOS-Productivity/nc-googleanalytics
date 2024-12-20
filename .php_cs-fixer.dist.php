49377b8f73d86d952d8610f30e9c0b94c47504d3
Misha M.-Kupriyanov <kupriyanov@strato.de>
2024-12-20 15:04:01 +0100

    chore(composer): add vendor-bin
    Signed-off-by: Misha M.-Kupriyanov <kupriyanov@strato.de>

--- .php_cs-fixer.dist.php
+++ .php_cs-fixer.dist.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 // SPDX-FileCopyrightText: Mikhailo Matiyenko-Kupriyanov <kupriyanov+nextcloud@strato.de>
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-require_once './vendor/autoload.php';
+require_once './vendor-bin/cs-fixer/vendor/autoload.php';
 
 use Nextcloud\CodingStandard\Config;
 
